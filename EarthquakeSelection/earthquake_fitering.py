848eb03e1b0a73cd703a5799a22986f877c5081c
Victor Calderon <vacalder@ncsu.edu>
2019-08-06 18:10:55 -0400

    Tuesday Update
    The code was modified to read Mainshocks and Aftershocks, still trying to find a way to automatically download Ground Motion files, it might be do because the page opens very quickly.

--- EarthquakeSelection/earthquake_fitering.py
+++ EarthquakeSelection/earthquake_fitering.py
@@ -10,6 +10,9 @@ import math  as math
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from bs4 import BeautifulSoup
 
 
 # EARTHQUAKE SELECTION PROGRAM
@@ -41,4 +44,44 @@ Peer_Filtered=PeerDB[all_filters]
 # Plotting Data Points
 
 Peer_Filtered.plot(kind='scatter',x='joyner_booredist_km',y='earthquake_magnitude')
-plt.show
+#plt.show
+
+# Selecting Mainshocks and generate string to download RSN
+
+dwnstr=''
+
+for x in range(0,2):
+    print('the valu of x is ',x)
+    Main_Shocks=Peer_Filtered[Peer_Filtered['earthquake_name'].str.contains('0'+str(x))]
+
+for i, row in Main_Shocks.iterrows():
+    dwnstr=','+str(row['record_sequence_number'])+dwnstr
+    
+MSstr=dwnstr[1:]
+# Accesing  PEER Website
+
+browser = webdriver.Chrome() 
+url="https://ngawest2.berkeley.edu/users/sign_in?unauthenticated=true"    
+browser.get(url)
+browser.find_element_by_id("user_email").send_keys('vacalder@ncsu.edu')
+browser.find_element_by_id("user_password").send_keys("ViCk2016")
+browser.find_element_by_id("user_submit").click()
+url2="https://ngawest2.berkeley.edu/spectras/223218/searches/new"
+browser.get(url2)
+browser.find_element_by_xpath('//*[@id="search_search_nga_number"]').send_keys(MSstr)
+browser.find_element_by_xpath('//*[@id="new_search"]/div[2]/fieldset/button').click()
+
+    
+# SelectiNG Aftershocks and generate String to download RSN
+
+    
+for y in range (2,3):
+    print('the valu of y is ',y)
+    Aftershocks=Peer_Filtered[Peer_Filtered['earthquake_name'].str.contains('0'+str(y))]
+    
+    
+for y in range (3,8):
+    print('the valu of y is ',y)
+    df=Peer_Filtered[Peer_Filtered['earthquake_name'].str.contains('0'+str(y))]
+    Aftershocks=Aftershocks.append(df)
+
